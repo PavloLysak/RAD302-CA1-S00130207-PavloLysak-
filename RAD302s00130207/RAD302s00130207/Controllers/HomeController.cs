d11615a43fca124f3d2c6ccb59cedd770c4aca01
PavloLysak <s00130207@mail.itsligo.ie>
2015-03-12 12:33:25 +0000

    1stCommit

---
+++ RAD302s00130207/RAD302s00130207/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using RAD302s00130207.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RAD302s00130207.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        public nwEntitites db = new nwEntitites();
+
+    
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View(db.Orders);
+        }
+    }
+}
---
+++ RAD302s00130207/README.md
@@ -0,0 +1,9 @@
+###Map Application for Nothwind
+Sample application to query Northwind and display the address and show a map(courtesy of Google Map API)
+
+
+###Steps
+
+*Make EF classes
+*Create API for Orders
+*Flesh out the UI
